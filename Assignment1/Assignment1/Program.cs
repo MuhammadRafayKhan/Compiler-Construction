9e36d3147d100ba5015b0fcc686cc016ce5aaccf
MuhammadRafayKhan <31164804+MuhammadRafayKhan@users.noreply.github.com>
2019-02-10 14:54:31 +0500

    Add files via upload

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+            string s1 = Console.ReadLine();
+
+            int i1 = 0;
+            float f1 = 0;
+            bool b1;
+
+            if (int.TryParse(s1, out i1))
+            {
+                Console.WriteLine("Your input {0} is Integer", s1);
+            }
+
+            else if (float.TryParse(s1, out f1))
+            {
+                Console.WriteLine("Your input {0} is Float", s1);
+            }
+
+            else if (bool.TryParse(s1, out b1))
+            {
+                Console.WriteLine("Your input {0} is bool", s1);
+            }
+
+            else
+            {
+                Console.WriteLine("Your input {0} is String", s1);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
